e6150444d814c939dc12e6bc4a95e5918944ede3
Aleksi Kalliokoski <a.kalliokoski@hotmail.com>
2023-11-01 20:43:50 +0200

    Added Material UI, Axios and Cors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,23 @@
+import { Button, Typography } from "@mui/material";
+import axios from "axios";
+import { useState } from "react";
+
 const App = () => {
-  return <div>Hello World!</div>;
+  const [buttonText, setButtonText] = useState<string>("Ping");
+
+  const handlePing = async () => {
+    const { data } = await axios.get<string>("http://localhost:4000/health");
+    setButtonText(data);
+  };
+
+  return (
+    <div>
+      <Typography>Hello World!</Typography>
+      <Button variant="contained" onClick={handlePing}>
+        {buttonText}
+      </Button>
+    </div>
+  );
 };
 
 export default App;

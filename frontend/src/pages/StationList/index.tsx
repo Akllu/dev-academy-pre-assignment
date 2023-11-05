109561a75b656d5e910f68f0f34430f436da7cc4
Aleksi Kalliokoski <a.kalliokoski@hotmail.com>
2023-11-05 18:46:27 +0200

    Frontend: made the station list page responsive

--- frontend/src/pages/StationList/index.tsx
+++ frontend/src/pages/StationList/index.tsx
@@ -1,8 +1,9 @@
-import { Button, Card } from "@mui/material";
+import { Button, Card, Grid } from "@mui/material";
 import { DataGrid, GridColDef, GridPaginationModel, GridRowSelectionModel } from "@mui/x-data-grid";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useGetStations from "../../hooks/useGetStations";
+import useIsMobile from "../../hooks/useIsMobile";
 import classes from "./StationList.module.css";
 
 const StationList = () => {
@@ -13,11 +14,12 @@ const StationList = () => {
   const [rowSelectionModel, setRowSelectionModel] = useState<GridRowSelectionModel>([]);
 
   const { loading, stations, totalRowCount } = useGetStations(paginationModel);
+  const { isMobile } = useIsMobile();
   const navigate = useNavigate();
 
   const columns: GridColDef[] = [
-    { field: "station_name", headerName: "Name", minWidth: 400 },
-    { field: "station_address", headerName: "Address", minWidth: 400 }
+    { field: "station_name", headerName: "Name", minWidth: isMobile ? 150 : 400 },
+    { field: "station_address", headerName: "Address", minWidth: isMobile ? 150 : 400 }
   ];
 
   const navigateToStation = () => {
@@ -29,27 +31,33 @@ const StationList = () => {
     <div className={classes.container}>
       <h1>Stations</h1>
       <Card sx={{ padding: "8px" }}>
-        <DataGrid
-          loading={loading}
-          columns={columns}
-          rows={stations}
-          rowSelectionModel={rowSelectionModel}
-          onRowSelectionModelChange={model => setRowSelectionModel(model)}
-          onRowDoubleClick={navigateToStation}
-          rowCount={totalRowCount}
-          pageSizeOptions={[5, 10, 25]}
-          paginationModel={paginationModel}
-          paginationMode="server"
-          onPaginationModelChange={model => setPaginationModel(model)}
-        />
+        <Grid container flexDirection={"column"} spacing={2}>
+          <Grid item xs={12}>
+            <DataGrid
+              loading={loading}
+              columns={columns}
+              rows={stations}
+              rowSelectionModel={rowSelectionModel}
+              onRowSelectionModelChange={model => setRowSelectionModel(model)}
+              onRowDoubleClick={navigateToStation}
+              rowCount={totalRowCount}
+              pageSizeOptions={[5, 10, 25]}
+              paginationModel={paginationModel}
+              paginationMode="server"
+              onPaginationModelChange={model => setPaginationModel(model)}
+            />
+          </Grid>
 
-        <Button
-          variant="contained"
-          sx={{ marginTop: "16px" }}
-          disabled={rowSelectionModel.length === 0}
-          onClick={navigateToStation}>
-          View the station
-        </Button>
+          <Grid item xs={12} lg={2}>
+            <Button
+              fullWidth
+              variant="contained"
+              disabled={rowSelectionModel.length === 0}
+              onClick={navigateToStation}>
+              View the station
+            </Button>
+          </Grid>
+        </Grid>
       </Card>
     </div>
   );

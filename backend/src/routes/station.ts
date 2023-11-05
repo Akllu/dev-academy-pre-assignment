277741f1fc88d0fd68cc915082738e31db52666b
Aleksi Kalliokoski <a.kalliokoski@hotmail.com>
2023-11-05 17:36:23 +0200

    Backend: modified the single station query to return related journey data

--- backend/src/routes/station.ts
+++ backend/src/routes/station.ts
@@ -24,8 +24,24 @@ router.get("/", async (req, res, next) => {
 
 router.get("/:id", async (req, res, next) => {
   try {
-    const id = Number(req.params.id);
-    const { rows } = await pool.query<IStation>("SELECT * FROM station WHERE id = $1", [id]);
+    const id = parseInt(req.params.id);
+    if (isNaN(id) || id < 0) {
+      throw new Error("Invalid id");
+    }
+
+    const { rows } = await pool.query<IStation>(
+      `
+    SELECT station.*,
+    COUNT(*) AS total_journeys_started,
+    (SELECT COUNT(*) FROM journey WHERE return_station_id = station.id) AS total_journeys_ended,
+    AVG(journey.distance) AS average_distance,
+    AVG(journey.duration) AS average_duration
+    FROM station
+    LEFT JOIN journey ON station.id = journey.departure_station_id
+    WHERE station.id = $1
+    GROUP BY station.id`,
+      [id]
+    );
     res.json(rows[0]);
   } catch (error) {
     next(error);
